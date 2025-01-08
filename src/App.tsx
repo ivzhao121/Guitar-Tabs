5b94655c42520b490edb92f54d1ba6d0799247cd
ivzhao121 <ivan_zhao@Ivans-MacBook-Pro-2.local>
2025-01-08 09:16:04 -0800

    bug fix and made canvas size dynamic

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import { useRef, useState, useEffect } from 'react'; 
 import html2canvas from 'html2canvas';
 import { Box, Button, IconButton, Stack } from '@mui/material';
-import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
-import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
-
+import AddIcon from '@mui/icons-material/Add';
+import RemoveIcon from '@mui/icons-material/Remove';
 import Vex from 'vexflow';
 import './App.css'
 import Typography from '@mui/material/Typography';
 import StringInput from './components/StringInput';
-import Sheet from './components/Sheet';
 
 interface Modifier {
   index: number, 
@@ -43,23 +41,29 @@ function calculateCurrentMeasureNumber(x: number, y:number, sheet: Vex.TabNote[]
   return count + y + 1; // add 1 because our y is 0 indexed
 }
 
+
 function App() {
   const { Renderer, TabStave, TabNote, TabTie, TabSlide, Formatter } = Vex.Flow;
+  const stave_width = (window.outerWidth - 48 - 10) / 3;
   const [sheet, setSheet] = useState<Vex.TabNote[][][]>([[[]]]) // stores the notes for each measure
-  const [staves, setStaves] = useState<Vex.Stave[][]>([[new TabStave(10, 50, 400)]]) // stores the object for each measure
+  const [staves, setStaves] = useState<Vex.Stave[][]>([[new TabStave(10, 0, stave_width)]]) // stores the object for each measure
   const tieStack = useRef<NoteTie[]>([]);
   const tiePair = useRef<PushedNote[]>([]);
   const [stave_number, setStaveNumber] = useState<number>(0) 
   const [measure_number, setMeasureNumber] = useState<number>(0)
-  const strings = useRef<string[]>(["-","-","-","-","-","-"])
+  const strings = useRef<string[]>(["","","","","",""])
   const stringTies = useRef<string[]>(["none", "none", "none", "none", "none", "none"])
   const exportDiv = useRef(null);
 
   useEffect(() => {
+          let canvas_height = 100;
+          for (let i = 0; i < staves.length; i++) {
+            canvas_height += staves[i][0].getHeight() + 20;
+          }
           const div = document.getElementById('output') as HTMLDivElement;
           div.innerHTML = "";
           const renderer = new Renderer(div, Renderer.Backends.SVG);
-          renderer.resize(window.innerWidth, window.outerHeight * 2); 
+          renderer.resize(window.outerWidth - 32, canvas_height); 
           const context = renderer.getContext();
           
           renderer.getContext().fillText("test", 0, 0);
@@ -106,7 +110,7 @@ function App() {
     const pos = [];
     const modifiers = [];
     for (let i = 0; i < 6; i++) {
-        if (strings.current[i] !== '-') {
+        if (strings.current[i] !== '') {
             pos.push({str: i + 1, fret: strings.current[i]}); // string positions are 1-indexed so we add 1
             if (stringTies.current[i] !== 'none') {
               modifiers.push({index: pos.length - 1, modifier: stringTies.current[i]});
@@ -147,11 +151,14 @@ function App() {
     if (prevSheet[stave_number][measure_number].length > 0) {
       const newGrid = prevSheet.map((rowArray) => [...rowArray]);
       const removedNote = newGrid[stave_number][measure_number].pop();
+      
+      const newTieStack = [];
       for (let i = 0; i < tieStack.current.length; i++) {
-        if (tieStack.current[i].firstNote === removedNote || tieStack.current[i].secondNote === removedNote) {
-          tieStack.current.splice(i, 1);
+        if (tieStack.current[i].firstNote !== removedNote && tieStack.current[i].secondNote !== removedNote) {
+          newTieStack.push(tieStack.current[i]);
         }
       }
+      tieStack.current = newTieStack;
       tiePair.current = [];
 
       setSheet(newGrid);
@@ -168,7 +175,7 @@ function App() {
         newStaves[stave_number + 1] = []
         newStaves[stave_number + 1][0] = new TabStave(newStaves[stave_number][0].getX(), 
                                                                     newStaves[stave_number][0].getHeight() + newStaves[stave_number][0].getY() + 20,  
-                                                                    400);
+                                                                    stave_width);
         setSheet(newSheet);
         setStaves(newStaves);
       }
@@ -190,7 +197,7 @@ function App() {
         newSheet[stave_number][measure_number + 1] = []
         newStaves[stave_number][measure_number + 1] = new TabStave(newStaves[stave_number][measure_number].getWidth() + newStaves[stave_number][measure_number].getX(), 
                                                                     newStaves[stave_number][measure_number].getY(),  
-                                                                    400);
+                                                                    stave_width);
         setSheet(newSheet);
         setStaves(newStaves);
       }
@@ -200,19 +207,72 @@ function App() {
     }
   }
 
+  function deleteCurrentMeasure() {
+    if (measure_number === 0 && stave_number === 0 && sheet.length === 1 && sheet[0].length === 1) {
+      setSheet([[[]]]);
+      setStaves([[new TabStave(10, 50, stave_width)]]);
+      tiePair.current = [];
+      tieStack.current = [];
+    } else {
+      const newSheet = sheet.map((rowArray) => [...rowArray]);
+      const newStaves = staves.map((rowArray) => [...rowArray]);
+      const width = staves[stave_number][measure_number].getWidth(); 
+      const removedNotes = newSheet[stave_number].splice(measure_number, 1)[0]; 
+      if (newSheet[stave_number].length === 0) {
+        for (let i = stave_number + 1; i < newStaves.length; i++) {
+          for (let j = 0; j < newStaves[i].length; j++) {
+            newStaves[i][j].setY(newStaves[i][j].getY() - newStaves[i][j].getHeight() - 20);
+          }
+        }
+        newSheet.splice(stave_number, 1);
+        newStaves.splice(stave_number, 1);
+      } else {
+        newStaves[stave_number].splice(measure_number, 1)
+        for (let i = measure_number; i < newStaves[stave_number].length; i++) {
+          newStaves[stave_number][i].setX(newStaves[stave_number][i].getX() - width);
+        }
+      }
+
+      const newTieStack = [] as NoteTie[];
+      for (let i = 0; i < tieStack.current.length; i++) {
+        if (removedNotes.map((note: Vex.TabNote) => tieStack.current[i].firstNote !== note && tieStack.current[i].secondNote !== note).every(Boolean)) {
+          newTieStack.push(tieStack.current[i]);
+        }
+      }
+
+      if (newTieStack.length !== 0)  {
+        console.log("yes");
+        tieStack.current = newTieStack;
+        console.log(tieStack.current.length);
+      }
+
+      tiePair.current = [];
+      setMeasureNumber(0);
+      setStaveNumber(0);
+      setSheet(newSheet);
+      setStaves(newStaves);
+    }
+
+  }
+
   const handleSaveAsImage = async () => {
     if (exportDiv.current) {
-      const canvas = await html2canvas(exportDiv.current);
-      const dataURL = canvas.toDataURL('image/png');
+      const canvas = await html2canvas(exportDiv.current, {
+        scrollX: -window.scrollX,
+        scrollY: -window.scrollY,
+        windowWidth: document.documentElement.offsetWidth,
+        windowHeight: document.documentElement.offsetHeight
+    });
+      const dataURL = canvas.toDataURL('my_tabs/png');
 
       // Create a link element and trigger a download
       const link = document.createElement('a');
       link.href = dataURL;
-      link.download = 'my-div.png';
+      link.download = 'my_tabs.png';
       link.click();
     }
   };
-
+  
   return (
     <>
     <Box padding={1}>
@@ -224,40 +284,43 @@ function App() {
         <StringInput inputLabel="Fifth-String" onFretChange={(fret) => strings.current[4] = fret} onTieChange={(tie) => stringTies.current[4] = tie}/>
         <StringInput inputLabel="Sixth-String" onFretChange={(fret) => strings.current[5] = fret} onTieChange={(tie) => stringTies.current[5] = tie}/>
         <Button type="submit" variant="contained" style={{margin: 10}}> Add</Button>
-        <Button variant="contained" onClick={(e) => deleteNote(e, sheet)} > Delete</Button>
+        <Button variant="contained" onClick={(e) => deleteNote(e, sheet)} > Delete</Button> 
+        <Button onClick={handleSaveAsImage} style = {{margin: 10}} > Save as Image </Button>
+
       </form>
-      <Stack alignItems="center" direction="column">
+      <Stack alignItems="center" direction="column" style={{marginBottom: 25}}>
         <Stack alignItems="center" direction="row" gap={2}>
           <IconButton onClick={() => changeStaveNumber(-1)}>
-            <KeyboardArrowLeftIcon />
+            <RemoveIcon />
           </IconButton>
           <Typography >
             Stave: {stave_number + 1} 
           </Typography>
           <IconButton onClick={() => changeStaveNumber(1)}>
-            <KeyboardArrowRightIcon />
+            <AddIcon />
           </IconButton>
         </Stack>
         <Stack alignItems="center" direction="row" gap={2}>
           <IconButton onClick={() => changeMeasureNumber(-1)}>
-            <KeyboardArrowLeftIcon />
+            <RemoveIcon />
           </IconButton>
           <Typography>
           Measure: {measure_number + 1}
           </Typography>
           <IconButton onClick={() => changeMeasureNumber(1)}>
-            <KeyboardArrowRightIcon />
+            <AddIcon />
           </IconButton>
         </Stack>
-        <Button onClick={handleSaveAsImage} > Save as Image </Button>
-    </Stack>
-    <div ref={exportDiv}>
-      <Box style={{margin: "auto", textAlign: "center", marginTop: 50}}>
-        <input style={{fontSize: 50, border: 0, textAlign: "center"}} placeholder="Title" />
+        <Button variant="contained" onClick={deleteCurrentMeasure} style={{backgroundColor: "red"}} > Delete Measure </Button>
+      </Stack>      
+      <hr /> 
+      <div ref={exportDiv} style={{marginTop: 25}}>
+        <Box style={{textAlign: "center"}}>
+          <input style={{fontSize: 20, border: 0, textAlign: "center", marginTop: 25}} placeholder="Title" />
+        </Box>
+        <div id="output" />
+      </div>
       </Box>
-      <div id="output" />
-    </div>
-    </Box>
     </>
   )
 }
